b11c913a345598ed35e27de10dc85a00a0facc9e
jaeseung <jhwang90801@gmail.com>
2022-06-08 17:35:55 +0900

    Modified(1): modeling
    - core/utils.py coding convention 적용

--- applications/models.py
+++ applications/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 from core.models import TimeStampModel
 
-class Status(models.Model):
+class Status(models.Model): 
     status = models.CharField(max_length=45)
 
-    class Meta:
+    class Meta: 
         db_table = "statuses"
 
-class Application(TimeStampModel):
+class Application(TimeStampModel): 
     status = models.ForeignKey('Status', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    job = models.ForeignKey('jobs.Job', on_delete=models.CASCADE)
+    user   = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    job    = models.ForeignKey('jobs.Job', on_delete=models.CASCADE)
     resume = models.ForeignKey('resumes.Resume', on_delete=models.CASCADE)
 
-    class Meta:
+    class Meta: 
         db_table = "applications"
 
--- resumes/models.py
+++ resumes/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from core.models import TimeStampModel
 
 class Resume(TimeStampModel):
-    name = models.CharField(max_length=50)
+    name     = models.CharField(max_length=50)
     file_url = models.URLField(max_length=300)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'resumes'
