d4cfbf990ef6df90bb1643a24f5aa7de9b8c26fd
Mia Lee <mia@Miaui-MacBookPro.local>
2022-06-09 18:05:16 +0900

    ADD : Resume file upload views.py 3

--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import json, boto3, uuid
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from resumes.models import Resume
+from my_settings    import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from core.utils     import signin_decorator
+
+# 파일 업로드
+class ResumeFileUploadView(View):
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id     = AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+        )
+    @signin_decorator
+    def post (self, request):
+        try:
+            user      = request.user
+            file      = request.FILES['resume']
+            file_uuid = str(uuid.uuid4())
+            file_url  = f"https://abstunator-wantus-resume-bucket.s3.us-west-2.amazonaws.com/{file_uuid}"
+
+            self.s3_client.upload_fileobj(
+                file,
+                "abstunator-wantus-resume-bucket",
+                file_uuid,
+                ExtraArgs = {
+                    "ContentType" : file.content_type
+                }
+            )
+
+            Resume.objects.create(
+            name     = file.name,
+            file_url = file_url,
+            user     = user)
+            
+            return JsonResponse({'message' : 'UPLOAD_SUCCESS'}, status=201) 
+
+        except KeyError:
+            return JsonResponse({'message' : "KEY_ERROR"}, status=400)
+
+
