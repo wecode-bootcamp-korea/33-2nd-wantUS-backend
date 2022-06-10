b574a132ab732c73136d26679c7351fc32737653
Mia Lee <mia@Miaui-MacBookPro.local>
2022-06-10 19:30:20 +0900

    CREATE : Resume file view  views.py 1 GET 요청을 사용한 이력서 조회 기능 구현

--- resumes/views.py
+++ resumes/views.py
@@ -42,3 +42,21 @@ class ResumeFileUploadView(View):
             return JsonResponse({'message' : "KEY_ERROR"}, status=400)
 
 
+class ResumeView(View):
+    s3_client = boto3.client(
+                's3',
+                aws_access_key_id = AWS_ACCESS_KEY_ID,
+                aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+                )
+    # 파일 조회 
+    @signin_decorator
+    def get (self,request,resume_id):
+
+        try: 
+            url = Resume.objects.get(id = resume_id).file_url
+
+            return JsonResponse({'message' : url}, status=200)
+        
+        except Resume.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_RESUME_ID'}, status=404)
+    
