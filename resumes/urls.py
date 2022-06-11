cb6ce5474912dc1edfbb96872bb76fc8c1a8ec15
Mia Lee <mia@Miaui-MacBookPro.local>
2022-06-11 16:02:19 +0900

    resumes app view s.py -- 이력서 리스트 기능 구현

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views      import ResumeFileUploadView, ResumeView
+from .views      import ResumeFileUploadView, ResumeView, ResumeListView
 
 urlpatterns = [
     path('', ResumeFileUploadView.as_view()),
     path('/list/<int:resume_id>', ResumeView.as_view()),
+    path('/list', ResumeListView.as_view()),
 ]
