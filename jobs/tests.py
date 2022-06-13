1b2a18e07c86cfb098e2bf77fdf0a023cfb1c1d0
jaeseung <jhwang90801@gmail.com>
2022-06-13 20:08:23 +0900

    CREATE: Follow View
    - 프론트엔드에서 필요한 데이터(프로필 이미지) 추가
    - MyPage, JobDetail Rebase compeleted
    - Follow 관련 기능 뷰 작성 완료
    - path parameter로 구현(end point 지정 완료)
    - 해당 채용 공고를 팔로우한 사람의 수 출력

--- jobs/tests.py
+++ jobs/tests.py
@@ -84,3 +84,89 @@ class JobDetailTest(TestCase):
         self.assertEqual(response.json(), {
             "message": "JOB_DOES_NOT_EXIST",
         })
+
+class FollowTest(TestCase):
+    def setUp(self):
+        Social.objects.create(
+            id   = 2,
+            name = 'kakao'
+        )
+        User.objects.create(
+            id                = 1,
+            name              = '김아무개',
+            profile_image     = 'http://k.kakaocdn.net/dn/eeTOt3/btrtLULVtEZ/5EXNjngEhg1QeAgUE3KKAk/img_640x640.jpg',
+            email             = 'kim1234@gmail.com',
+            social_account_id = '12345678',
+            social_id         = Social.objects.get(name="kakao").id,
+        )
+        Location.objects.create(
+            id   = 1,
+            name = "서울"
+        )
+        Company.objects.create(
+            id          = 1,
+            name        = "위코드",
+            address     = "서울시 송파구 오륜동",
+            logo        = "https://i.pinimg.com/564x/f6/16/1d/f6161d61d9223223fcad406a632fa828.jpg",
+            location_id = 1,
+            latitude    = "37.1234",
+            longitude   = "123.1234"
+        )
+        MainCategory.objects.create(
+            id   = 1,
+            name = "개발"
+        )
+        SubCategory.objects.create(
+            id               = 1,
+            name             = "앱 개발자",
+            main_category_id = 1,
+        )
+        Career.objects.create(
+            id   = 1,
+            name = "신입",
+        )
+        Job.objects.create(
+            id              = 1,
+            name            = "위코드 개발자",
+            content         = "내용",
+            due_date        = "2022-06-30",
+            career_id       = 1,
+            company_id      = 1,
+            sub_category_id = 1
+        )
+        self.token = jwt.encode({"id" : User.objects.get(id=1).id}, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Social.objects.all().delete()
+        Location.objects.all().delete()
+        Company.objects.all().delete()
+        MainCategory.objects.all().delete()
+        SubCategory.objects.all().delete()
+        Career.objects.all().delete()
+        Job.objects.all().delete()
+
+    def test_follow_success(self): 
+        client   = Client()
+        headers  = {"HTTP_Authorization" : self.token}
+        response = client.post("/job/1/follow", **headers, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "message"     : "FOLLOW_SUCCESS",
+            "results"     : True,
+            "follow_count": 1
+        })
+
+    def test_unfollow_success(self): 
+        client = Client()
+        Follow.objects.create(id=1, job_id=1, user_id=1)
+        headers  = {"HTTP_Authorization" : self.token}
+        response = client.post("/job/1/follow", **headers, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "message"     : "FOLLOW_DELETE",
+            "results"     : False,
+            "follow_count": 0
+        })
--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from jobs.views import JobDetailView
+from jobs.views  import JobDetailView, FollowView
 
 urlpatterns = [
     path("/<int:job_id>", JobDetailView.as_view()),
-]
+    path("/<int:job_id>/follow", FollowView.as_view()),
+] 
