3414a7677900a208f8c4f4c68b2f311064c39f1e
Mia Lee <mia@Miaui-MacBookPro.local>
2022-06-14 10:53:44 +0900

    ADD : jobs app, views.py 채용 공고 리스트 페이지 구현 수정2

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from jobs.views  import JobDetailView, FollowView, FollowedJobView
+from jobs.views  import FollowView, FollowedJobView, JobListPublicView, JobListPrivateView
 
 urlpatterns = [
-    path("/<int:job_id>", JobDetailView.as_view()),
     path("/<int:job_id>/follow", FollowView.as_view()),
-    path("/followedjob", FollowedJobView.as_view())
+    path("/followedjob", FollowedJobView.as_view()),
+    path('/public', JobListPublicView.as_view()),
+    path('/private', JobListPrivateView.as_view())
 ] 
