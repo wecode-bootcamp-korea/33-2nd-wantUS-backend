d616f6da25ccce5a84e3aac21820b73b026dcbe9
jaeseung <jhwang90801@gmail.com>
2022-06-15 10:18:04 +0900

    CREATE: followed Job View
    - 팔로우한 채용공고 리스트 확인
    - select_related() ORM 최적화 완료
    - 프론트엔드와 통신 확인 완료
    - Code convention 체크
    - unittest 확인 완료

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from jobs.views  import JobDetailView, FollowView
+from jobs.views  import JobDetailView, FollowView, FollowedJobView
 
 urlpatterns = [
     path("/<int:job_id>", JobDetailView.as_view()),
     path("/<int:job_id>/follow", FollowView.as_view()),
+    path("/followedjob", FollowedJobView.as_view())
 ] 
