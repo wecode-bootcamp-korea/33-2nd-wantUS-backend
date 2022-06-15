49ac0a9e5e87f91717e8676e0272da05a113bada
jaeseung <jhwang90801@gmail.com>
2022-06-15 21:52:10 +0900

    MODIFIED: Job List View & ADD: Random List View
    - Job List View 수정(Q 객체 수정, 리스트 출력값 수정)
    - ORM 최적화 진행

--- jobs/views.py
+++ jobs/views.py
@@ -11,72 +11,11 @@ from jobs.models import *
 from users.models import *
 from core.utils import signin_decorator
 
-
-class JobListPublicView(View):
-    def get(self, request):
-        offset        = request.GET.get('offset', 0)
-        limit         = request.GET.get('limit', 10)
-        sort          = request.GET.get('sort', 'most_recent')
-        main_category = request.GET.get('category')
-        sub_category  = request.GET.get('sub_category')
-        location      = request.GET.get('location')
-        career        = request.GET.get('career')
-        skill         = request.GET.get('skill')
-        search        = request.GET.get('search')
-
-
-        sort_dict = {
-            'most_recent' : '-id',
-            'popularity' : '-like_num'
-        }
-
-        q = Q()
-
-        if main_category:
-            q.add(Q(sub_category__main_category = main_category), q.AND)
-
-        if sub_category:
-            q.add(Q(sub_category = sub_category), q.AND)
-
-        if location:
-            q.add(Q(location__name_in = location), q.AND)
-        
-        if career and career:
-            q.add(Q(career_gte= career), q.AND)
-
-        if skill:
-            q.add(Q(skill__name_in = skill), q.AND)
-        
-        if search:
-            q &= Q(name__icontains = search)
-
-        job_list = Job.objects.filter(q).annotate(like_num = Count('follow'))\
-            .order_by(sort_dict[sort])[offset:offset+limit]
-
-
-        results = [{
-            'name': job.name,
-            'category' : job.sub_category.name,
-            'location' : job.company.location.name,
-            'careerYear' : job.career.name,
-            'stack' : [{
-                'id': skill.id,
-                'name' : skill.name
-            } for skill in Skill.objects.filter(requiredskill__job_id=job.id)],
-            'follow' :  False,
-            'company_image' : [{
-                'id' : company_image.id,
-                'image_url' : company_image.image_url
-            } for company_image in job.company.companyimage_set.all()]
-        }for job in job_list]
-
-        return JsonResponse({'result' : results}, status=200)
-
 class JobListPrivateView(View):
     @signin_decorator
     def get(self, request):
         offset        = int(request.GET.get('offset', 0))
-        limit         = int(request.GET.get('limit', 10))
+        limit         = int(request.GET.get('limit', 8))
         sort          = request.GET.get('sort', 'most_recent')
         main_category = request.GET.get('category')
         sub_category  = request.GET.get('sub_category')
@@ -86,50 +25,57 @@ class JobListPrivateView(View):
         search        = request.GET.get('search')
 
         sort_dict = {
-            'most_recent' : '-id',
+            'most_recent' : '-due_date',
             'popularity' : '-like_num'
         }
 
         q = Q()
 
         if main_category:
-            q.add(Q(sub_category__main_category = main_category), q.AND)
+            q.add(Q(sub_category__main_category_id = main_category), q.AND)
 
         if sub_category:
-            q.add(Q(sub_category = sub_category), q.AND)
+            q.add(Q(sub_category_id = sub_category), q.AND)
 
         if location:
-            q.add(Q(location__name_in = location), q.AND)
+            q.add(Q(company__location_id = location), q.AND)
         
         if career and career:
-            q.add(Q(career_gte= career), q.AND)
+            q.add(Q(career_id= career), q.AND)
 
         if skill:
-            q.add(Q(skill__name_in = skill), q.AND)
+            q.add(Q(requiredskill__skill__id= skill), q.AND)
         
         if search:
             q &= Q(name__icontains = search)
         
-        job_list = Job.objects.filter(q).annotate(like_num = Count('follow'))\
+        job_list = Job.objects.select_related('company', 'company__location', 'career').filter(q).annotate(like_num = Count('follow'))\
             .order_by(sort_dict[sort])[offset:offset+limit]
 
         results = [{
-            'name': job.name,
-            'category' : job.sub_category.name,
-            'location' : job.company.location.name,
-            'careerYear' : job.career.name,
-            'stack' : [{
-                'id': skill.id,
-                'name' : skill.name
-            } for skill in Skill.objects.filter(requiredskill__job_id=job.id)],
-            'follow' : Follow.objects.filter(user_id=request.user.id, job_id=job.id).exists(),
-            'company_image' : [{
-                'id' : company_image.id,
-                'image_url' : company_image.image_url
-            } for company_image in job.company.companyimage_set.all()]
+            'id'      : job.id,
+            'name'    : job.name,
+            'company' : job.company.name,
+            'location': job.company.location.name,
+            'years'   : job.career.name,
+            'follow'  : Follow.objects.filter(user_id=request.user.id, job_id=job.id).exists(),
+            'url'     : job.company.companyimage_set.first().image_url
         } for job in job_list]
         
         return JsonResponse({'result' : results}, status=200)
+
+class RandomJobListView(View):
+    def get(self, request):
+        jobs    = Job.objects.all().order_by("?")
+        results = [{
+            'id'      : job.id,
+            'name'    : job.name,
+            'company' : job.company.name,
+            'location': job.company.location.name,
+            'image'   : job.company.companyimage_set.first().image_url
+        } for job in jobs]
+        
+        return JsonResponse({'results' : results}, status=200)
 
 class FollowView(View):
     @signin_decorator
@@ -158,10 +104,10 @@ class FollowedJobView(View):
         user = request.user
         follows = Follow.objects.select_related('job', 'job__company', 'job__company__location').filter(user_id = user.id)
         followed_jobs = [{
-            'position' : follow.job.name,
-            'company_name' : follow.job.company.name,
-            'location' : follow.job.company.location.name,
-            'image' : follow.job.company.companyimage_set.first().image_url
+            'position'    : follow.job.name,
+            'company_name': follow.job.company.name,
+            'location'    : follow.job.company.location.name,
+            'image'       : follow.job.company.companyimage_set.first().image_url
         } for follow in follows]
 
         return JsonResponse({"results" : followed_jobs}, status=200)
