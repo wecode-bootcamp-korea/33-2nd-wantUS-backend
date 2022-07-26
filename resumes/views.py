ff039506c84c92a56a67b57318e9eeff7e987ab3
jaeseung <jhwang90801@gmail.com>
2022-07-26 20:16:42 +0900

    Add Dockerfile & Update ResumeView

--- resumes/views.py
+++ resumes/views.py
@@ -1,79 +1,99 @@
-import json, uuid, boto3
+import boto3
 
-from django.http  import JsonResponse, HttpResponse
 from django.views import View
+from django.http  import JsonResponse
+from django.conf  import settings
+from pathlib      import Path
 
-from resumes.models import Resume
-from my_settings    import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from core.utils     import signin_decorator
-
-class ResumeFileUploadView(View):
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id     = AWS_ACCESS_KEY_ID,
-        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
-    )
-    @signin_decorator
-    def post (self, request):
-        try:
-            user = request.user
-            file = request.FILES['resume']
-            key  = str(uuid.uuid4())
-
-            self.s3_client.upload_fileobj(
-                file,
-                "abstunator-wantus-resume-bucket",
-                key,
-                ExtraArgs = {
-                    "ContentType" : file.content_type
-                }
-            )
-
-            Resume.objects.create(
-                name     = file.name,
-                file_url = key,
-                user     = user
-            )
-
-            return JsonResponse({'message' : 'UPLOAD_SUCCESS'}, status=201) 
-
-        except KeyError:
-            return JsonResponse({'message' : "KEY_ERROR"}, status=400)
+from users.models   import User
+from resumes.models import Resume
 
 class ResumeView(View):
     s3_client = boto3.client(
-        's3',
-        aws_access_key_id = AWS_ACCESS_KEY_ID,
-        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+        's3', 
+        aws_access_key_id     = settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
     )
-    
-    @signin_decorator
-    def delete (self,request, resume_id):
-        try:
 
-            resume    = Resume.objects.get(id=resume_id)
-            key       = resume.file_url 
-            s3_client = boto3.client(
-                's3',
-                aws_access_key_id     = AWS_ACCESS_KEY_ID,
-                aws_secret_access_key = AWS_SECRET_ACCESS_KEY
-            )
-            s3_client.delete_object(Bucket='abstunator-wantus-resume-bucket', Key=key)
+    @signin_decorator
+    def post(self, request):
+        user = request.user.id
+        file = request.FILES['resume']
+        if file.name.endswith('.pdf') == False:
+            return JsonResponse({"message" : "CAN_ONLY_UPLOAD_PDF"}, status=400)
+
+        file_name = str(file)
+
+        self.s3_client.upload_fileobj(
+            file, 
+            settings.AWS_STORAGE_BUCKET_NAME, 
+            file_name, 
+            ExtraArgs = {"ContentType": file.content_type}
+        )
+
+        Resume(
+            name     = file.name,
+            file_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{file_name}",
+            user_id  = user
+        ).save()
+
+        return JsonResponse({"message" : "UPLOAD_SUCCESS"}, status=201)
+
+    @signin_decorator
+    def delete(self, request, resume_id):
+        try:
+            user      = request.user
+            resume    = Resume.objects.get(id = resume_id)
+            file_url  = resume.file_url
+            file_name = file_url[54:]
+
+            if resume.user.id !=user:
+                return JsonResponse({"message" : "UNAUTHORIZED_USER"}, status=401)
+
+            self.s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=file_name)
             resume.delete()
 
-            return HttpResponse(204)
+            return JsonResponse({"message" : "DELETE_SUCCESS"}, status=200)
 
         except Resume.DoesNotExist:
-            return JsonResponse({'message' : 'BAD_REQUEST'}, status=404)
+            return JsonResponse({"message" : "RESUME_DOES_NOT_EXIST"}, status=404)
+
+class ResumeDownloadView(View):
     
-# 파일 리스트
+    s3_client = boto3.client(
+        's3', 
+        aws_access_key_id     = settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+    )
+    @signin_decorator
+    def get(self, request, resume_id):
+        try:
+            user      = request.user
+            resume    = Resume.objects.get(id=resume_id)
+            file_name = resume.file_url[54:]
+
+            if resume.user.id != user:
+                return JsonResponse({"message" : "UNAUTHORIZED_USER"}, status=401)
+
+            self.s3_client.download_file(
+                settings.AWS_STORAGE_BUCKET_NAME,
+                file_name,
+                str(Path.home() / f"Downloads/{file_name}"),
+            )
+
+            return JsonResponse({"message" : "DOWNLOAD_SUCCESS"}, status=200)
+
+        except Resume.DoesNotExist:
+            return JsonResponse({"message" : "RESUME_DOES_NOT_EXIST"}, status=404)
+    
 class ResumeListView(View):
     @signin_decorator
     def get(self, request):
 
         results = [{
-            "id" : resume.id,
-            "user" : resume.user.name,
+            "id"          : resume.id,
+            "user"        : resume.user.name,
             "name"        : resume.name,
             "created_date": resume.created_at.strftime('%Y.%m.%d')
         } for resume in Resume.objects.filter(user=request.user)]
